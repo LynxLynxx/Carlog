1c529d05016531c2af1067ac0cd7a219362fc30a
j_rydzewski <52713164+janrydzewski@users.noreply.github.com>
2024-05-11 20:36:02 +0200

    connectivity backend

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:carlog/core/addons/bloc_observer.dart';
 import 'package:carlog/core/di/injectable_config.dart';
 import 'package:carlog/core/router/router.dart';
 import 'package:carlog/features/auth_features/auth/auth_bloc.dart';
+import 'package:carlog/features/other_features/error/presentation/cubit/network_connection_cubit.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,11 +10,18 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
-  await Firebase.initializeApp(
-  );
+  await Firebase.initializeApp();
   configureDependencies();
-  runApp(BlocProvider.value(
-    value: locator<AuthBloc>()..add(const AuthEvent.getUserSession()),
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider.value(
+        value: locator<AuthBloc>()..add(const AuthEvent.getUserSession()),
+      ),
+      BlocProvider(
+        create: (context) => NetworkConnectionCubit(),
+        lazy: false,
+      ),
+    ],
     child: const MyApp(),
   ));
 }
