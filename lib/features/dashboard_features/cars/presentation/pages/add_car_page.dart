13f4dc976a0f2bd545d1f1db16e82e572427afd9
j_rydzewski <52713164+janrydzewski@users.noreply.github.com>
2024-06-21 20:42:50 +0200

    car brand list

--- lib/features/dashboard_features/cars/presentation/pages/add_car_page.dart
+++ lib/features/dashboard_features/cars/presentation/pages/add_car_page.dart
@@ -9,6 +9,7 @@ import 'package:carlog/features/dashboard_features/cars/domain/entities/car_enti
 import 'package:carlog/features/dashboard_features/cars/presentation/bloc/add_car/manage_car_bloc.dart';
 import 'package:carlog/features/dashboard_features/cars/presentation/bloc/basic_add_car/basic_add_car_bloc.dart';
 import 'package:carlog/features/dashboard_features/cars/presentation/bloc/cars/cars_bloc.dart';
+import 'package:carlog/features/dashboard_features/cars/presentation/widgets/custom_alphabet_scroll.dart';
 import 'package:carlog/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -81,6 +82,8 @@ class _AddCarViewState extends State<AddCarView>
     super.dispose();
   }
 
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     final carList = CarEntity.fromMap(JsonsK.cars);
@@ -103,100 +106,70 @@ class _AddCarViewState extends State<AddCarView>
                 ),
               ],
             ),
-            Padding(
-              padding: PaddingsK.h20,
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 150,
-                    child: LottieBuilder.asset(
-                      AnimationsK.paintCar,
-                      controller: _controller,
-                      onLoaded: (composition) {
-                        _controller.duration = composition.duration;
-                      },
-                    ),
+            Expanded(
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.95,
+                child: Padding(
+                  padding: PaddingsK.h20,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                        height: 150,
+                        child: LottieBuilder.asset(
+                          AnimationsK.paintCar,
+                          controller: _controller,
+                          onLoaded: (composition) {
+                            _controller.duration = composition.duration;
+                          },
+                        ),
+                      ),
+                      TweenAnimationBuilder<double>(
+                        duration: Durations.long1,
+                        curve: Curves.easeInOut,
+                        tween: Tween<double>(
+                          begin: 0,
+                          end: 1,
+                        ),
+                        builder: (context, value, _) => LinearProgressIndicator(
+                          value: _currentFrame / _totalFrames,
+                          borderRadius: PaddingsK.circular30,
+                          minHeight: 8,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.45,
+                        child: CustomAlphabetScroll(
+                          list: carList,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Unable to locate your brand or model?",
+                          style: text16W500LS1.copyWith(fontSize: 14),
+                        ),
+                      ),
+                      GestureDetector(
+                        //
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Enter it manually!",
+                            style: text16W500LS1.copyWith(
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  TweenAnimationBuilder<double>(
-                    duration: Durations.long1,
-                    curve: Curves.easeInOut,
-                    tween: Tween<double>(
-                      begin: 0,
-                      end: 1,
-                    ),
-                    builder: (context, value, _) => LinearProgressIndicator(
-                      value: _currentFrame / _totalFrames,
-                      borderRadius: PaddingsK.circular30,
-                      minHeight: 8,
-                    ),
-                  ),
-
-                  // const Text(
-                  //   "Car Brand",
-                  //   style: text22W700LS3,
-                  // ),
-                  // BlocBuilder<BasicAddCarBloc, BasicAddCarState>(
-                  //   builder: (context, state) {
-                  //     return SizedBox(
-                  //       height: 70,
-                  //       width: double.infinity,
-                  //       child: ListView.builder(
-                  //         scrollDirection: Axis.horizontal,
-                  //         itemBuilder: (context, index) => GestureDetector(
-                  //             onTap: () => context.read<BasicAddCarBloc>().add(
-                  //                 BasicAddCarEvent.changeBrand(brandId: index)),
-                  //             child: CarListElement(
-                  //                 isSelected: index == state.brandId,
-                  //                 text: carList[index].brand)),
-                  //         itemCount: carList.length,
-                  //       ),
-                  //     );
-                  //   },
-                  // ),
-                  // const Text(
-                  //   "Model",
-                  //   style: text22W700LS3,
-                  // ),
-                  // BlocBuilder<BasicAddCarBloc, BasicAddCarState>(
-                  //   builder: (context, state) {
-                  //     return SizedBox(
-                  //       height: 70,
-                  //       width: double.infinity,
-                  //       child: ListView.builder(
-                  //         scrollDirection: Axis.horizontal,
-                  //         itemBuilder: (context, index) => GestureDetector(
-                  //             onTap: () => context.read<BasicAddCarBloc>().add(
-                  //                 BasicAddCarEvent.changeModel(modelId: index)),
-                  //             child: CarListElement(
-                  //                 isSelected: index == state.modelId,
-                  //                 text: carList[state.brandId].models[index])),
-                  //         itemCount: carList[state.brandId].models.length,
-                  //       ),
-                  //     );
-                  //   },
-                  // ),
-                  // const SizedBox(
-                  //   height: 50,
-                  // ),
-                  // Container(
-                  //   alignment: Alignment.center,
-                  //   child: Text(
-                  //     "Unable to locate your brand or model?",
-                  //     style: text16W500LS1.copyWith(fontSize: 14),
-                  //   ),
-                  // ),
-                  // GestureDetector(
-                  //   //
-                  //   child: Container(
-                  //     alignment: Alignment.center,
-                  //     child: Text(
-                  //       "Enter it manually!",
-                  //       style:
-                  //           text16W500LS1.copyWith(fontWeight: FontWeight.w700),
-                  //     ),
-                  //   ),
-                  // ),
-                ],
+                ),
               ),
             ),
           ],
---
+++ lib/features/dashboard_features/cars/presentation/widgets/custom_alphabet_scroll.dart
@@ -0,0 +1,236 @@
+// ignore_for_file: depend_on_referenced_packages, must_be_immutable, library_private_types_in_public_api
+
+import 'package:carlog/core/constants/paddings.dart';
+import 'package:carlog/features/dashboard_features/cars/domain/entities/car_entity.dart';
+import 'package:collection/collection.dart' show IterableExtension;
+import 'package:flutter/material.dart';
+
+class CustomAlphabetScroll extends StatefulWidget {
+  const CustomAlphabetScroll({
+    super.key,
+    required this.list,
+    this.isAlphabetsFiltered = true,
+    this.itemExtent = 75,
+  });
+
+  final List<CarEntity> list;
+
+  final double itemExtent;
+
+  final bool isAlphabetsFiltered;
+
+  @override
+  _AlphabetScrollViewState createState() => _AlphabetScrollViewState();
+}
+
+class _AlphabetScrollViewState extends State<CustomAlphabetScroll> {
+  void init() {
+    widget.list
+        .sort((x, y) => x.brand.toLowerCase().compareTo(y.brand.toLowerCase()));
+    _list = widget.list;
+    setState(() {});
+
+    if (widget.isAlphabetsFiltered) {
+      List<String> temp = [];
+      for (var letter in alphabets) {
+        CarEntity? firstAlphabetElement = _list.firstWhereOrNull((item) =>
+            item.brand.toLowerCase().startsWith(letter.toLowerCase()));
+        if (firstAlphabetElement != null) {
+          temp.add(letter);
+        }
+      }
+      _filteredAlphabets = temp;
+    } else {
+      _filteredAlphabets = alphabets;
+    }
+    calculateFirstIndex();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    init();
+    if (listController.hasClients) {
+      maxScroll = listController.position.maxScrollExtent;
+    }
+    super.initState();
+  }
+
+  ScrollController listController = ScrollController();
+  final _selectedIndexNotifier = ValueNotifier<int>(0);
+  final positionNotifer = ValueNotifier<Offset>(const Offset(0, 0));
+  final Map<String, int> firstIndexPosition = {};
+  List<String> _filteredAlphabets = [];
+  final letterKey = GlobalKey();
+  List<CarEntity> _list = [];
+  bool isLoading = false;
+  bool isFocused = false;
+  final key = GlobalKey();
+
+  @override
+  void didUpdateWidget(covariant CustomAlphabetScroll oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.list != widget.list ||
+        oldWidget.isAlphabetsFiltered != widget.isAlphabetsFiltered) {
+      _list.clear();
+      firstIndexPosition.clear();
+      init();
+    }
+  }
+
+  int getCurrentIndex(double vPosition) {
+    double kAlphabetHeight = letterKey.currentContext!.size!.height;
+    return (vPosition ~/ kAlphabetHeight);
+  }
+
+  void calculateFirstIndex() {
+    for (var letter in _filteredAlphabets) {
+      CarEntity? firstElement = _list.firstWhereOrNull(
+          (item) => item.brand.toLowerCase().startsWith(letter));
+      if (firstElement != null) {
+        int index = _list.indexOf(firstElement);
+        firstIndexPosition[letter] = index;
+      }
+    }
+  }
+
+  void scrolltoIndex(int x, Offset offset) {
+    int index = firstIndexPosition[_filteredAlphabets[x].toLowerCase()]!;
+    final scrollToPostion = widget.itemExtent * index - 10 * index;
+    listController.animateTo((scrollToPostion),
+        duration: const Duration(milliseconds: 300), curve: Curves.easeOut);
+    positionNotifer.value = offset;
+  }
+
+  void onVerticalDrag(Offset offset) {
+    int index = getCurrentIndex(offset.dy);
+    if (index < 0 || index >= _filteredAlphabets.length) return;
+    _selectedIndexNotifier.value = index;
+    setState(() {
+      isFocused = true;
+    });
+    scrolltoIndex(index, offset);
+  }
+
+  double? maxScroll;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ListView.builder(
+          controller: listController,
+          scrollDirection: Axis.vertical,
+          itemCount: _list.length,
+          itemBuilder: (context, index) {
+            return ConstrainedBox(
+              constraints: BoxConstraints(maxHeight: widget.itemExtent),
+              child: Padding(
+                padding: const EdgeInsets.only(right: 40),
+                child: Container(
+                  padding: PaddingsK.all16,
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: PaddingsK.circular10,
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                  ),
+                  child: Text(
+                    _list[index].brand,
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color:
+                              Theme.of(context).colorScheme.onPrimaryContainer,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: Container(
+            key: key,
+            padding: const EdgeInsets.symmetric(horizontal: 2),
+            child: SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              child: GestureDetector(
+                onVerticalDragStart: (z) => onVerticalDrag(z.localPosition),
+                onVerticalDragUpdate: (z) => onVerticalDrag(z.localPosition),
+                onVerticalDragEnd: (z) {
+                  setState(() {
+                    isFocused = false;
+                  });
+                },
+                child: ValueListenableBuilder<int>(
+                    valueListenable: _selectedIndexNotifier,
+                    builder: (context, int selected, Widget? child) {
+                      return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: List.generate(
+                            _filteredAlphabets.length,
+                            (x) => GestureDetector(
+                              key: x == selected ? letterKey : null,
+                              onTap: () {
+                                _selectedIndexNotifier.value = x;
+                                scrolltoIndex(x, positionNotifer.value);
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 12, vertical: 2),
+                                child: Text(
+                                  _filteredAlphabets[x].toUpperCase(),
+                                  style: selected == x
+                                      ? Theme.of(context)
+                                          .textTheme
+                                          .titleLarge!
+                                          .copyWith(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .primary,
+                                              fontWeight: FontWeight.w800)
+                                      : Theme.of(context).textTheme.bodyLarge!,
+                                ),
+                              ),
+                            ),
+                          ));
+                    }),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+const List<String> alphabets = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
