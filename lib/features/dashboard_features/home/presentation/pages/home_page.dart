6bc6ec54e79b3bd346966fa55dbff5ec51ffda0d
j_rydzewski <52713164+janrydzewski@users.noreply.github.com>
2024-06-29 13:26:14 +0200

    expandable button

--- lib/features/dashboard_features/home/presentation/pages/home_page.dart
+++ lib/features/dashboard_features/home/presentation/pages/home_page.dart
@@ -1,36 +1,48 @@
-import 'dart:developer';
-
+import 'package:carlog/core/di/injectable_config.dart';
+import 'package:carlog/features/dashboard_features/cars/presentation/bloc/cars/cars_bloc.dart';
 import 'package:carlog/features/dashboard_features/home/presentation/widgets/home_app_bar.dart';
 import 'package:carlog/features/dashboard_features/shared/widgets/dashboard_appbar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CarsBloc(locator())..add(const CarsEvent.getCars()),
+      child: const HomeView(),
+    );
+  }
 }
 
-class _HomePageState extends State<HomePage> {
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomeView> {
   bool isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
     return DashboardAppbar.appbar(
       appBar: homeAppBar(
-        context,
-        isExpanded,
-        () {
-          log("Turn");
-          setState(() => isExpanded = !isExpanded);
-        },
-      ),
-      body: Column(
-        children: [
-          Center(
-            child: TextButton(
+          context,
+          isExpanded,
+          () => setState(() {
+                isExpanded = !isExpanded;
+              })),
+      body: AnimatedSize(
+        duration: Durations.extralong1,
+        child: Column(
+          children: [
+            TextButton(
               child: Column(
                 children: [
                   const Text("ASD"),
@@ -41,8 +53,8 @@ class _HomePageState extends State<HomePage> {
               ),
               onPressed: () => context.push("/add"),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/features/dashboard_features/home/presentation/widgets/home_app_bar.dart
+++ lib/features/dashboard_features/home/presentation/widgets/home_app_bar.dart
@@ -3,14 +3,19 @@ import 'package:carlog/core/constants/images.dart';
 import 'package:carlog/core/constants/paddings.dart';
 import 'package:carlog/core/extensions/styles_extenstion.dart';
 import 'package:carlog/core/theme/styles/button_styles.dart';
+import 'package:carlog/features/dashboard_features/cars/presentation/bloc/cars/cars_bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
 SliverAppBar homeAppBar(
-        BuildContext context, bool isExpanded, Function() func) =>
+  BuildContext context,
+  bool isExpanded,
+  Function() func,
+) =>
     SliverAppBar(
-      toolbarHeight: 120,
+      toolbarHeight: isExpanded ? 350 : 330,
       flexibleSpace: Container(
         padding: PaddingsK.h20,
         decoration: BoxDecoration(
@@ -19,126 +24,183 @@ SliverAppBar homeAppBar(
               bottomLeft: Radius.circular(30),
               bottomRight: Radius.circular(30)),
         ),
-        child: Column(
-          children: [
-            const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Hello again,",
-                      style: context.titleMedium!
-                          .copyWith(color: context.onSurface),
-                    ),
-                    Text(
-                      "${FirebaseAuth.instance.currentUser?.displayName ?? "Joe"}!",
-                      style: context.titleLarge!
-                          .copyWith(color: context.onSurface),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    SvgPicture.asset(
-                      ImagesK.search,
-                      width: 30,
-                      height: 30,
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    SvgPicture.asset(
-                      ImagesK.bell,
-                      width: 30,
-                      height: 30,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Stack(
-              children: [
-                Container(
-                  height: 50,
-                  margin: PaddingsK.h20,
-                  decoration: BoxDecoration(
-                    color: context.surfaceDim,
-                    borderRadius: PaddingsK.circular30,
-                  ),
-                  padding: PaddingsK.h20v10,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Row(
-                        children: [
-                          Text(
-                            "Opel",
-                            style: context.headlineSmall!.copyWith(
-                              color: context.onPrimaryContainer,
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            "Insignia",
-                            style: context.titleMedium!.copyWith(
-                              color: context.secondaryColor,
-                            ),
-                          ),
-                        ],
+                      Text(
+                        "Hello again,",
+                        style: context.titleMedium!
+                            .copyWith(color: context.onSurface),
                       ),
-                      Row(
-                        children: [
-                          Text(
-                            "173418",
-                            style: context.headlineSmall!
-                                .copyWith(color: context.onPrimaryContainer),
-                          ),
-                          Text(
-                            "KM",
-                            style: context.labelSmall!.copyWith(
-                              color: context.secondaryColor,
-                            ),
-                          ),
-                        ],
+                      Text(
+                        "${FirebaseAuth.instance.currentUser?.displayName ?? "Joe"}!",
+                        style: context.titleLarge!
+                            .copyWith(color: context.onSurface),
                       ),
                     ],
                   ),
-                ),
-                Positioned(
-                  left: 150,
-                  right: 150,
-                  bottom: 0,
-                  child: Transform.translate(
-                    offset: const Offset(0, 25),
-                    child: ElevatedButton(
-                      style: homeCarButton(context),
-                      onPressed: func,
-                      child: AnimatedRotation(
-                        duration: DurationsK.d250,
-                        turns: isExpanded ? -0.5 : 0.0,
-                        child: Image.asset(
-                          "assets/icons/arrow.png",
-                          fit: BoxFit.fitHeight,
-                        ),
+                  Row(
+                    children: [
+                      SvgPicture.asset(
+                        ImagesK.search,
+                        width: 30,
+                        height: 30,
                       ),
-                    ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      SvgPicture.asset(
+                        ImagesK.bell,
+                        width: 30,
+                        height: 30,
+                      ),
+                    ],
                   ),
-                ),
-                
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-          ],
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              DropDownWidget(
+                isExpanded: isExpanded,
+                func: func,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
         ),
       ),
     );
+
+class DropDownWidget extends StatelessWidget {
+  final bool isExpanded;
+  final Function() func;
+  const DropDownWidget({
+    super.key,
+    required this.isExpanded,
+    required this.func,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        BlocBuilder<CarsBloc, CarsState>(
+          builder: (context, state) {
+            if (state.carList.isEmpty) {
+              return const CircularProgressIndicator();
+            }
+            return AnimatedContainer(
+                duration: Durations.extralong1,
+                height: isExpanded ? 250 : 60,
+                margin: PaddingsK.h20,
+                decoration: BoxDecoration(
+                  color: isExpanded
+                      ? context.primaryContainer
+                      : context.surfaceDim,
+                  borderRadius: PaddingsK.circular30,
+                ),
+                padding: PaddingsK.h20v10,
+                child: isExpanded
+                    ? SingleChildScrollView(
+                        child: Column(
+                          children: state.carList
+                              .map(
+                                  (toElement) => const CarSelectElementWidget())
+                              .toList(),
+                        ),
+                      )
+                    : const Column(
+                        children: [
+                          CarSelectElementWidget(),
+                        ],
+                      ));
+          },
+        ),
+        Positioned(
+          left: 150,
+          right: 150,
+          bottom: 0,
+          child: Transform.translate(
+            offset: const Offset(0, 25),
+            child: ElevatedButton(
+              style: homeCarButton(context),
+              onPressed: func,
+              child: AnimatedRotation(
+                duration: DurationsK.d250,
+                turns: isExpanded ? -0.5 : 0.0,
+                child: Image.asset(
+                  "assets/icons/arrow.png",
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CarSelectElementWidget extends StatelessWidget {
+  const CarSelectElementWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Text(
+                "Opel",
+                style: context.headlineSmall!.copyWith(
+                  color: context.onPrimaryContainer,
+                ),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Text(
+                "Insignia",
+                style: context.titleMedium!.copyWith(
+                  color: context.secondaryColor,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                "173418",
+                style: context.headlineSmall!
+                    .copyWith(color: context.onPrimaryContainer),
+              ),
+              Text(
+                "KM",
+                style: context.labelSmall!.copyWith(
+                  color: context.secondaryColor,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
