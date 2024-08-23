5faf5736d90bd81fa6aae4a4af81df68b6bfb4e1
j_rydzewski <52713164+janrydzewski@users.noreply.github.com>
2024-08-23 13:13:46 +0200

    action bloc rebuilt

--- lib/features/dashboard_features/cars/presentation/bloc/action/action_state.dart
+++ lib/features/dashboard_features/cars/presentation/bloc/action/action_state.dart
@@ -2,9 +2,9 @@ part of 'action_bloc.dart';
 
 @freezed
 class ActionState with _$ActionState {
-  const factory ActionState([
-    @Default(FormzSubmissionStatus.initial) FormzSubmissionStatus status,
-    @Default([]) List<CarActionDayEntity> carActionDayEntity,
-    String? message,
-  ]) = _ActionState;
+  const factory ActionState.initial() = _Initial;
+  const factory ActionState.loading() = _Loading;
+  const factory ActionState.data(List<CarActionDayEntity> carActionDayEntity) =
+      _Data;
+  const factory ActionState.failure(Failure failure) = _Failure;
 }
--- lib/features/dashboard_features/home/presentation/widgets/timeline/user_action_board_widget.dart
+++ lib/features/dashboard_features/home/presentation/widgets/timeline/user_action_board_widget.dart
@@ -2,11 +2,12 @@ import 'package:carlog/core/constants/animations.dart';
 import 'package:carlog/core/constants/paddings.dart';
 import 'package:carlog/core/router/routes_constants.dart';
 import 'package:carlog/features/dashboard_features/cars/presentation/bloc/action/action_bloc.dart';
+import 'package:carlog/features/dashboard_features/home/domain/entities/car_action_day_entity.dart';
 import 'package:carlog/features/dashboard_features/home/presentation/widgets/timeline/timeline_widget.dart';
 import 'package:carlog/shared/widgets/carlog_loader.dart';
+import 'package:carlog/shared/widgets/error_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:formz/formz.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lottie/lottie.dart';
 
@@ -15,56 +16,62 @@ class UserActionBoardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<ActionBloc, ActionState>(
-      listener: (context, state) {},
+    return BlocBuilder<ActionBloc, ActionState>(
       builder: (context, state) {
-        if (state.status.isInProgress) {
-          return const CarlogLoader();
-        }
-        if (!state.status.isFailure) {
-          if (state.carActionDayEntity.isEmpty) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 100),
-                LottieBuilder.asset(AnimationsK.emptyCalendar),
-                const SizedBox(
-                  height: 50,
-                ),
-                GestureDetector(
-                  onTap: () => context.push(RoutesK.addAction, extra: context),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: Padding(
-                      padding: PaddingsK.h30v10,
-                      child: Card(
-                        child: Container(
-                            margin: PaddingsK.all16,
-                            child: const Icon(Icons.add)),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            );
-          }
-          return Padding(
-            padding: PaddingsK.h20,
-            child: ListView.builder(
-              shrinkWrap: true,
-              padding: PaddingsK.v20,
-              physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) => TimelineWidget(
-                isFirst: index == 0,
-                isLast: index == state.carActionDayEntity.length - 1,
-                carActionDayEntity: state.carActionDayEntity[index],
-              ),
-              itemCount: state.carActionDayEntity.length,
-            ),
-          );
-        }
-        return const SizedBox.shrink();
+        return state.when(
+          initial: () => const CarlogLoader(),
+          loading: () => const CarlogLoader(),
+          data: (carList) => _buildBody(carList, context),
+          failure: (failure) => ErrorIndicator(failure: failure),
+        );
       },
     );
   }
+
+  _buildBody(
+      List<CarActionDayEntity> carActionDayEntityList, BuildContext context) {
+    if (carActionDayEntityList.isEmpty) {
+      return _buildEmptyActions(context);
+    }
+    return _buildActionList(carActionDayEntityList);
+  }
+
+  _buildEmptyActions(BuildContext context) => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 100),
+          LottieBuilder.asset(AnimationsK.emptyCalendar),
+          const SizedBox(
+            height: 50,
+          ),
+          GestureDetector(
+            onTap: () => context.push(RoutesK.addAction, extra: context),
+            child: SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: PaddingsK.h30v10,
+                child: Card(
+                  child: Container(
+                      margin: PaddingsK.all16, child: const Icon(Icons.add)),
+                ),
+              ),
+            ),
+          ),
+        ],
+      );
+
+  _buildActionList(List<CarActionDayEntity> carActionDayEntityList) => Padding(
+        padding: PaddingsK.h20,
+        child: ListView.builder(
+          shrinkWrap: true,
+          padding: PaddingsK.v20,
+          physics: const NeverScrollableScrollPhysics(),
+          itemBuilder: (context, index) => TimelineWidget(
+            isFirst: index == 0,
+            isLast: index == carActionDayEntityList.length - 1,
+            carActionDayEntity: carActionDayEntityList[index],
+          ),
+          itemCount: carActionDayEntityList.length,
+        ),
+      );
 }
