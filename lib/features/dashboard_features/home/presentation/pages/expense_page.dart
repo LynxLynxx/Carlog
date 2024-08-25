7f393d356c7f1f5943d94486007eaa2028475729
j_rydzewski <52713164+janrydzewski@users.noreply.github.com>
2024-08-25 22:07:35 +0200

    model

---
+++ lib/features/dashboard_features/home/presentation/pages/expense_page.dart
@@ -0,0 +1,143 @@
+import 'package:carlog/core/constants/formats.dart';
+import 'package:carlog/core/constants/paddings.dart';
+import 'package:carlog/core/di/injectable_config.dart';
+import 'package:carlog/features/dashboard_features/cars/presentation/bloc/action/action_bloc.dart';
+import 'package:carlog/features/dashboard_features/cars/presentation/bloc/manage_action/manage_action_bloc.dart';
+import 'package:carlog/features/dashboard_features/cars/presentation/widgets/add_car/list_element_textfield_widget.dart';
+import 'package:carlog/features/dashboard_features/home/domain/entities/car_action_enum.dart';
+import 'package:carlog/features/dashboard_features/home/presentation/widgets/action/address_picker_widget.dart';
+import 'package:carlog/features/dashboard_features/home/presentation/widgets/action/custom_dropdown_widget.dart';
+import 'package:carlog/features/dashboard_features/home/presentation/widgets/action/date_picker_widget.dart';
+import 'package:carlog/features/other_features/user_app/presentation/bloc/user_app_bloc.dart';
+import 'package:carlog/generated/l10n.dart';
+import 'package:carlog/shared/widgets/carlog_bottom_button_widget.dart';
+import 'package:carlog/shared/widgets/carlog_scaffold.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
+import 'package:go_router/go_router.dart';
+
+class ExpensePage extends StatelessWidget {
+  final BuildContext appContext;
+  const ExpensePage({
+    super.key,
+    required this.appContext,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ManageActionBloc(locator(), locator(), locator(),
+          context.read<ActionBloc>(), context.read<UserAppBloc>()),
+      child: const ExpenseView(),
+    );
+  }
+}
+
+class ExpenseView extends StatefulWidget {
+  const ExpenseView({super.key});
+
+  @override
+  State<ExpenseView> createState() => _ActionViewState();
+}
+
+class _ActionViewState extends State<ExpenseView> {
+  final addressEditingController = TextEditingController();
+  final dateEditingController = TextEditingController();
+  final noteEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    addressEditingController.dispose();
+    dateEditingController.dispose();
+    noteEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: CarlogScaffold.title(
+        title: S.of(context).addAction,
+        body: BlocConsumer<ManageActionBloc, ManageActionState>(
+          listener: (context, state) {
+            addressEditingController.text = state.address.value;
+            dateEditingController.text =
+                state.date != null ? FormatsK.yyyyMMdd.format(state.date!) : "";
+            noteEditingController.text = state.note.value;
+          },
+          builder: (context, state) {
+            return Column(
+              children: [
+                Padding(
+                  padding: PaddingsK.all16,
+                  child: Column(
+                    children: [
+                      const CustomDropdownWidget(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      state.action != CarActionEnum.note
+                          ? Column(
+                              children: [
+                                AddressPickerWidget(
+                                    textEditingController:
+                                        addressEditingController,
+                                    state: state),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                              ],
+                            )
+                          : const SizedBox.shrink(),
+                      DatePickerWidget(
+                          textEditingController: dateEditingController,
+                          state: state),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ListElementTextfieldWidget(
+                          textEditingController: noteEditingController,
+                          func: (value) {
+                            context
+                                .read<ManageActionBloc>()
+                                .add(ManageActionEvent.changeNote(value));
+                          },
+                          maxLines: 6,
+                          title: S.of(context).note,
+                          hintText: S.of(context).egRememberToChangeTheOil,
+                          displayError: state.note.displayError ?? ""),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+        bottomWidget: BlocSelector<ManageActionBloc, ManageActionState, bool>(
+          selector: (state) {
+            return state.status.isInProgress;
+          },
+          builder: (context, state) {
+            final manageActionState = context.watch<ManageActionBloc>().state;
+            return CarlogBottomButtonWidget(
+              title: S.of(context).save,
+              isLoading: state,
+              isActive: manageActionState.action != CarActionEnum.note
+                  ? manageActionState.address.value.isNotEmpty &&
+                      manageActionState.date != null
+                  : manageActionState.date != null,
+              onTap: () {
+                context
+                    .read<ManageActionBloc>()
+                    .add(const ManageActionEvent.submitActionEvent());
+                context.pop();
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
