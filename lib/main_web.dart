e10ca57d8b0935d57753b15ababdbedbb6b14299
Ryszard Schossler <ryszard.schossler@gmail.com>
2024-10-02 14:35:54 +0200

    change

--- lib/main_web.dart
+++ lib/main_web.dart
@@ -25,10 +25,6 @@ import 'generated/l10n.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   final firebaseConfig = js.context['firebaseConfig'];
   if (firebaseConfig == null) {
     await Firebase.initializeApp(
